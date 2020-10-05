236f50963808ba85b2d5accd0b7c8a6d7bd94bbe
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-10-05 22:14:44 +0200

    tests pass for new parameter generator

--- runner/parameter_generator.py
+++ runner/parameter_generator.py
@@ -4,6 +4,8 @@ import os
 import sys
 import yaml
 import json
+from copy import deepcopy
+from datetime import datetime
 
 import pandas as pd
 from pyDOE2 import lhs
@@ -17,115 +19,154 @@ from typing import List, Optional
 
 default_config_file = Path(__file__).parent.parent / "run_configs/config_example.yaml"
 
+all_groups = [
+    "pub",
+    "grocery",
+    "cinema",
+    "city_transport",
+    "inter_city_transport",
+    "hospital",
+    "care_home",
+    "company",
+    "school",
+    "household",
+    "university",
+]
+
+
+def iter_paths(d):
+    def iter1(d, path):
+        paths = []
+        for k, v in d.items():
+            if isinstance(v, dict):
+                paths += iter1(v, path + [k])
+            paths.append((path + [k], v))
+        return paths
+
+    return iter1(d, [])
+
+
+def set_value_in_path(d, path, value):
+    i = 0
+    while i < len(path) - 1:
+        if path[i] not in d:
+            d[path[i]] = {}
+        d = d[path[i]]
+        i += 1
+    d[path[-1]] = value
+
 
 class ParameterGenerator:
     def __init__(
-        self,
-        parameter_list: List[dict],
-        parameters_to_fix: Optional[dict] = None,
-        parameters_to_run: List[int] = "all",
+        self, parameter_list: List[dict], parameters_to_run: List[int] = "all",
     ):
-        self._check_sanity(parameter_list, parameters_to_fix)
+        self.parameter_list = self._read_parameter_list(parameter_list)
         self.parameter_list = parameter_list
-        for i, parameters in enumerate(self.parameter_list):
-            parameters["run_number"] = i
-            if parameters_to_fix is not None:
-                for key, value in parameters_to_fix.items():
-                    parameters[key] = value
         self.parameters_to_run = self._read_parameters_to_run(parameters_to_run)
 
-    @classmethod
-    def from_file(cls, path_to_parameters: str, parameters_to_fix: Optional[dict] = None, parameters_to_run="all"):
-        parameter_list = pd.read_csv(path_to_parameters).to_dict("records")
-        return cls(parameter_list=parameter_list, parameters_to_fix=parameters_to_fix, parameters_to_run=parameters_to_run)
+    def _read_parameter_list(self, parameter_list):
+        """
+        Reads the parameter list. If there is any file marked as lockdown
+        then we build a config for soft and hard lockdown using the parameters specified.
+        See the runner tests for examples.
+        """
+        for parameters in parameter_list:
+            if "policies" in parameters:
+                for policy in parameters["policies"]:
+                    if "lockdown" in policy:
+                        ret = deepcopy(parameters["policies"])
+                        lockdown_config = ret.pop("lockdown")
+                        lockdown_policies = build_policy_config_for_lockdown(
+                            **lockdown_config
+                        )
+                        parameters["policies"] = {**ret, **lockdown_policies}
 
     @classmethod
-    def from_grid(cls, parameter_dict: List[dict], parameters_to_fix: Optional[dict] = None, parameters_to_run="all"):
-        keys, values = zip(*parameter_dict.items())
-        permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
-        return cls(
-            parameter_list=permutations_dicts, parameters_to_fix = parameters_to_fix, parameters_to_run=parameters_to_run
-        )
+    def from_file(
+        cls, path_to_parameters: str, parameters_to_run="all",
+    ):
+        with open(path_to_parameters, "r") as f:
+            parameter_list = json.load(f)
+        return cls(parameter_list=parameter_list, parameters_to_run=parameters_to_run,)
 
     @classmethod
-    def from_regular_grid(cls, parameter_dict: List[dict], parameters_to_fix: Optional[dict] = None, parameters_to_run="all"):
-        for key, value in parameter_dict.items():
-            parameter_dict[key] = np.linspace(
-                start=value[0], stop=value[1], num=value[2]
-            )
-        return cls.from_grid(
-            parameter_dict=parameter_dict, parameters_to_fix=parameters_to_fix, parameters_to_run=parameters_to_run
-        )
-
-    @classmethod
-    def from_latin_hypercube(cls, parameter_bounds, n_samples, parameters_to_fix: Optional[dict] = None, parameters_to_run="all"):
-        return cls(
-            parameter_list=cls._generate_lhs(
-                cls, parameter_bounds=parameter_bounds, n_samples=n_samples
-            ),
-            parameters_to_fix = parameters_to_fix,
-            parameters_to_run=parameters_to_run,
-        )
-
-    def _check_sanity(self, parameters_to_vary, parameters_to_fix):
-        # Check that in list of parameters all have the same keys
-        counter_list = []
-        for parameter in parameters_to_vary:
-            # check a key is not repeated within same row
-            counter = Counter(list(parameter.keys()))
-            try:
-                for key, value in counter.items():
-                    assert value == 1
-            except:
-                raise ValueError("There are rows with repeated parameters!")
-            counter_list.append(counter)
-        try:
-            unique_counter = [
-                dict(s) for s in set(frozenset(c.items()) for c in counter_list)
-            ]
-            assert len(unique_counter) == 1
-        except:
-            raise ValueError(
-                "some of the parameter configurations dont have the same parameters"
-            )
-
-        # Check that parameters_to_fix are not included in parameters_to_vary otherwise error
-        try:
-            if parameters_to_fix is not None:
-                print(f"Parameters being fixed = ", parameters_to_fix.keys())
-                assert (
-                    set(parameters_to_fix.keys()) & set(parameters_to_vary[0].keys())
-                    == set()
-                )
+    def from_grid(
+        cls, parameter_dict: List[dict], parameters_to_run="all",
+    ):
+        paths = []
+        value_ranges = []
+        fixed_parameters = {}
+        for path, value in iter_paths(parameter_dict):
+            if type(value) == dict:
+                continue
+            elif type(value) == list:
+                paths.append(path)
+                value_ranges.append(value)
             else:
-                print(f"No fixed parameters")
-        except:
-            raise ValueError(
-                "Check what you are doing! You are varying some of the parameters that you are fixing and I can tell you that it is a bad idea"
-            )
-        # Print what parameters are being varied and what parameters are fixed
-        print(f"Paramters being varied = ", parameters_to_vary[0].keys())
+                set_value_in_path(fixed_parameters, path, value)
+        parameter_list = []
+        permutations = itertools.product(*value_ranges)
+        for permutation in permutations:
+            ret = deepcopy(fixed_parameters)
+            for path, value in zip(paths, permutation):
+                set_value_in_path(ret, path=path, value=value)
+            parameter_list.append(ret)
+        return cls(parameter_list=parameter_list, parameters_to_run=parameters_to_run,)
 
-    def _generate_lhs(self, parameter_bounds, n_samples, seed=1):
+    @classmethod
+    def from_regular_grid(
+        cls, parameter_dict: List[dict], parameters_to_run="all",
+    ):
+        ret = {}
+        for path, value in iter_paths(parameter_dict):
+            if type(value) == dict:
+                continue
+            elif type(value) == list:
+                if len(value) != 3:
+                    raise ValueError(f"For regular grid lists must have size 3")
+                set_value_in_path(ret, path, list(np.linspace(*value)))
+            else:
+                set_value_in_path(ret, path, value)
+        return cls.from_grid(parameter_dict=ret, parameters_to_run=parameters_to_run,)
+
+    @classmethod
+    def from_latin_hypercube(
+        cls, parameter_dict: List[dict], n_samples, parameters_to_run="all",
+    ):
+        paths = []
+        value_ranges = []
+        fixed_parameters = {}
+        for path, value in iter_paths(parameter_dict):
+            if type(value) == dict:
+                continue
+            elif type(value) == list:
+                paths.append(path)
+                value_ranges.append(value)
+            else:
+                set_value_in_path(fixed_parameters, path, value)
+        parameter_list = []
+        sampled_parameters = cls._generate_lhs(
+            parameter_bounds=value_ranges, n_samples=n_samples
+        )
+        for parameter_values in sampled_parameters:
+            ret = deepcopy(fixed_parameters)
+            for path, value in zip(paths, parameter_values):
+                set_value_in_path(ret, path=path, value=value)
+            parameter_list.append(ret)
+        return cls(parameter_list=parameter_list, parameters_to_run=parameters_to_run,)
+
+    @classmethod
+    def _generate_lhs(cls, parameter_bounds, n_samples, seed=1):
         """
         Generates a latin hypercube array.
         """
-        bounds = list(parameter_bounds.values())
-        num_vars = len(bounds)
+        num_vars = len(parameter_bounds)
         lhs_array = lhs(
             n=num_vars, samples=n_samples, criterion="maximin", random_state=seed
         )
         # scale to the bounds
-        scale_samples(lhs_array, bounds)
-        parameter_dicts = []
-        for i in range(len(lhs_array)):
-            parameter_dicts.append(
-                {
-                    key: value
-                    for key, value in zip(parameter_bounds.keys(), lhs_array[i])
-                }
-            )
-        return parameter_dicts
+        scale_samples(lhs_array, parameter_bounds)
+        return lhs_array
 
     def _read_parameters_to_run(self, parameters_to_run):
         if parameters_to_run is None:
@@ -147,3 +188,48 @@ class ParameterGenerator:
 
     def __iter__(self):
         return iter([self[idx] for idx in self.parameter_list])
+
+
+def build_policy_config_for_lockdown(
+    soft_lockdown_date: str,
+    hard_lockdown_date: str,
+    lockdown_ratio: int,
+    hard_lockdown_policy_parameters: dict,
+):
+    policy_dict = {}
+    for policy_type in hard_lockdown_policy_parameters:
+        policy_dict[policy_type] = {
+            "1": {"start_time": soft_lockdown_date},
+            "2": {"start_time": hard_lockdown_date},
+        }
+        # soft first
+        if policy_type == "social_distancing":
+            overall_beta_factor = hard_lockdown_policy_parameters["social_distancing"][
+                "overall_beta_factor"
+            ]
+            policy_dict[policy_type]["1"]["beta_factors"] = {}
+            policy_dict[policy_type]["2"]["beta_factors"] = {}
+            for group in all_groups:
+                if group == "household":
+                    policy_dict[policy_type]["1"]["beta_factors"][group] = 1.0
+                    policy_dict[policy_type]["2"]["beta_factors"][group] = 1.0
+                    continue
+                policy_dict[policy_type]["1"]["beta_factors"][
+                    group
+                ] = 1 + lockdown_ratio * (overall_beta_factor - 1)
+                policy_dict[policy_type]["2"]["beta_factors"][
+                    group
+                ] = overall_beta_factor
+        elif policy_type == "quarantine":
+            overall_compliance = hard_lockdown_policy_parameters["quarantine"][
+                "overall_compliance"
+            ]
+            policy_dict[policy_type]["1"]["compliance"] = (
+                lockdown_ratio * overall_compliance
+            )
+            policy_dict[policy_type]["1"]["household_compliance"] = (
+                lockdown_ratio * overall_compliance
+            )
+            policy_dict[policy_type]["2"]["compliance"] = overall_compliance
+            policy_dict[policy_type]["2"]["household_compliance"] = overall_compliance
+    return policy_dict
